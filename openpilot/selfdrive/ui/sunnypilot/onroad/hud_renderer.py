5bcec27ad563a778143d621c031333324d253888
Cristian Zantedeschi <c.zantedeschi@gmail.com>
2026-09-19 09:04:13 +0200

    feat(nnslr): render VISION separately from operational SLA

--- openpilot/selfdrive/ui/sunnypilot/onroad/hud_renderer.py
+++ openpilot/selfdrive/ui/sunnypilot/onroad/hud_renderer.py
@@ -12,6 +12,7 @@ from openpilot.selfdrive.ui.sunnypilot.onroad.developer_ui import DeveloperUiRen
 from openpilot.selfdrive.ui.sunnypilot.onroad.road_name import RoadNameRenderer
 from openpilot.selfdrive.ui.sunnypilot.onroad.rocket_fuel import RocketFuel
 from openpilot.selfdrive.ui.sunnypilot.onroad.speed_limit import SpeedLimitRenderer
+from openpilot.selfdrive.ui.sunnypilot.onroad.speed_vision import VisionSpeedLimitRenderer
 from openpilot.selfdrive.ui.sunnypilot.onroad.smart_cruise_control import SmartCruiseControlRenderer
 from openpilot.selfdrive.ui.sunnypilot.onroad.turn_signal import TurnSignalController
 from openpilot.selfdrive.ui.sunnypilot.onroad.circular_alerts import CircularAlertsRenderer
@@ -32,6 +33,7 @@ class HudRendererSP(HudRenderer):
     self.road_name_renderer = RoadNameRenderer()
     self.rocket_fuel = RocketFuel()
     self.speed_limit_renderer = SpeedLimitRenderer()
+    self.speed_vision_renderer = VisionSpeedLimitRenderer()
     self.smart_cruise_control_renderer = SmartCruiseControlRenderer()
     self.turn_signal_controller = TurnSignalController()
     self.circular_alerts_renderer = CircularAlertsRenderer()
@@ -56,6 +58,7 @@ class HudRendererSP(HudRenderer):
     super()._update_state()
     self.road_name_renderer.update()
     self.speed_limit_renderer.update()
+    self.speed_vision_renderer.update()
     self.smart_cruise_control_renderer.update()
     self.turn_signal_controller.update()
     self.circular_alerts_renderer.update()
@@ -140,6 +143,7 @@ class HudRendererSP(HudRenderer):
     self.developer_ui.render(rect)
     self.road_name_renderer.render(rect)
     self.speed_limit_renderer.render(rect)
+    self.speed_vision_renderer.render(rect)
     self.smart_cruise_control_renderer.render(rect)
     self.turn_signal_controller.render(rect)
     self.circular_alerts_renderer.render(rect)
