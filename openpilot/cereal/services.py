20096de119c41ec7def31c5cb2bd4e979cb3af1f
Cristian Zantedeschi <c.zantedeschi@gmail.com>
2026-09-19 08:51:46 +0200

    feat(nnslr): register non-logged speed vision service

--- openpilot/cereal/services.py
+++ openpilot/cereal/services.py
@@ -92,6 +92,7 @@ _services: dict[str, tuple] = {
   "carStateSP": (True, 100., 10),
   "liveMapDataSP": (True, 1., 1),
   "modelDataV2SP": (True, 20., None, QueueSize.BIG),
+  "speedVisionState": (False, 10., None),
   "liveLocationKalman": (True, 20.),
 
   # debug
